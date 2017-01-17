4e0d54878e192a5f70d728c36706f94489f6ab41
李铭昕 <715557344@qq.com>
2017-01-17 13:31:49 +0800

    * 【新增】lumen模板测试 接口测试

---
+++ lumen_v5.3/app/Http/Controllers/TestController.php
@@ -0,0 +1,34 @@
+<?php
+// +----------------------------------------------------------------------
+// | Demo [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016 http://www.lmx0536.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: limx <715557344@qq.com> <http://www.lmx0536.cn>
+// +----------------------------------------------------------------------
+// | Date: 2017/1/17 Time: 下午1:17
+// +----------------------------------------------------------------------
+namespace App\Http\Controllers;
+
+class TestController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+        return response()->json(['name' => 'Abigail', 'state' => 'CA']);
+    }
+
+    public function view()
+    {
+        return view('index');
+    }
+}
--- lumen_v5.3/routes/web.php
+++ lumen_v5.3/routes/web.php
@@ -14,3 +14,6 @@
 $app->get('/', function () use ($app) {
     return $app->version();
 });
+
+$app->get('/test/index', 'TestController@index');
+$app->get('/test/view', 'TestController@view');
