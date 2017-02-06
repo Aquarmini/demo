2b3ee3d1feb231a37e38db29751382cca308431b
李铭昕 <715557344@qq.com>
2017-02-06 11:44:54 +0800

    * 增加静态方法转化DEMO

--- test/demo1.php
+++ test/demo1.php
@@ -6,8 +6,23 @@ class test
     {
         echo "HELLO WORLD" . PHP_EOL;
     }
+
+    public function hello2()
+    {
+        echo "HELLO WORLD 2" . PHP_EOL;
+    }
+
+    public static function __callStatic($name, $arguments)
+    {
+        $cls = new self();
+        call_user_func_array([$cls], [$name, $arguments]);
+    }
 }
+
 
 $a = new test();
 $a->hello();
 test::hello();
+
+$a->hello2();
+test::hello2();
