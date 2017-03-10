16b5460f97757626c852384e2c67f7f2bfd35bd2
李铭昕 <715557344@qq.com>
2017-03-10 10:26:24 +0800

    * 更新phalcon

--- phalcon/app/config/app.php
+++ phalcon/app/config/app.php
@@ -12,6 +12,9 @@ return [
     'project-name' => 'limx-phalcon-ss-project',
     // 定时执行的脚本
     'cron-tasks' => [
+        ['task' => 'Test\\Test1', 'action' => 'main', 'params' => [], 'time' => '10:02'],
+        ['task' => 'Test\\Test2', 'action' => 'main', 'params' => [1], 'time' => ''],
+
         // ['task' => 'System\\Cron', 'action' => 'test', 'params' => [], 'time' => '05:21'],
     ],
     'error-code' => [
--- phalcon/app/config/loader.php
+++ phalcon/app/config/loader.php
@@ -9,9 +9,14 @@ $loader
     ->registerNamespaces(
         [
             'MyApp\Controllers' => $config->application->controllersDir,
+            'MyApp\Controllers\Admin' => $config->application->controllersDir . 'admin/',
+            'MyApp\Controllers\Test' => $config->application->controllersDir . 'test/',
             'MyApp\Models' => $config->application->modelsDir,
+            'MyApp\Models\Test' => $config->application->modelsDir . 'test/',
             'MyApp\Tasks' => $config->application->tasksDir,
             'MyApp\Tasks\System' => $config->application->tasksDir . 'system/',
+            'MyApp\Tasks\Test' => $config->application->tasksDir . 'test/',
+            'MyApp\Tasks\Async' => $config->application->tasksDir . 'async/',
             'MyApp\Traits' => $config->application->traitsDir,
             'MyApp\Traits\System' => $config->application->traitsDir . 'system/',
             'MyApp\Listeners' => $config->application->listenersDir,
