6766949310af3ec2e6a051cf4de029875b3db1b5
李铭昕 <715557344@qq.com>
2017-03-11 16:42:34 +0800

    * 增加nodejs测试

---
+++ test/compare.js
@@ -0,0 +1,20 @@
+/**
+ * Created by limx on 2017/3/11.
+ */
+if ([] >= []) {
+    console.log("[]>=[]成立！");
+} else {
+    console.log("[]>=[]不成立！");
+}
+
+if ([] <= []) {
+    console.log("[]<=[]成立！");
+} else {
+    console.log("[]<=[]不成立！");
+}
+
+if ([] == []) {
+    console.log("[]==[]成立！");
+} else {
+    console.log("[]==[]不成立！");
+}
