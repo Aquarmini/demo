e529d94c246fe679df0b5a033c2e0a224619965b
李铭昕 <715557344@qq.com>
2017-03-17 16:48:52 +0800

    * 更新lumen5.4

---
+++ lumen_v5.4/app/Http/Controllers/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class IndexController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+        echo 1;
+    }
+}
