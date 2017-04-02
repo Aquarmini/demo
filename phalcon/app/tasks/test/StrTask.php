d5cd1725659030db10913fc97d0248cb1a4d0b54
李铭昕 <715557344@qq.com>
2017-04-02 13:32:01 +0800

    * phalcon

---
+++ phalcon/app/tasks/test/StrTask.php
@@ -0,0 +1,37 @@
+<?php
+// +----------------------------------------------------------------------
+// | StrTask php 函数参数相关测试 [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016 http://www.lmx0536.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: limx <715557344@qq.com> <http://www.lmx0536.cn>
+// +----------------------------------------------------------------------
+namespace MyApp\Tasks\Test;
+
+use limx\phalcon\Utils\Str;
+use Phalcon\Cli\Task;
+use limx\phalcon\Cli\Color;
+
+class StrTask extends Task
+{
+    public function mainAction()
+    {
+        echo Color::head('Help:') . PHP_EOL;
+        echo Color::colorize('  PHP函数参数测试') . PHP_EOL . PHP_EOL;
+
+        echo Color::head('Usage:') . PHP_EOL;
+        echo Color::colorize('  php run Test\\\\Str [action]', Color::FG_GREEN) . PHP_EOL . PHP_EOL;
+
+        echo Color::head('Actions:') . PHP_EOL;
+        echo Color::colorize('  random      {$1}        随机字符串', Color::FG_GREEN) . PHP_EOL;
+    }
+
+    public function randomAction($params)
+    {
+        $num = rand(1, 10);
+        if (count($params) > 0) {
+            $num = intval($params[0]);
+        }
+        echo Color::colorize(Str::random($num), Color::FG_GREEN) . PHP_EOL;
+    }
+}
