08f63ff2c9e436e54b1ec7d7b2b101a95ca9376d
李铭昕 <715557344@qq.com>
2017-04-11 16:19:42 +0800

    * phalcon

--- phalcon/app/tasks/test/RedisTask.php
+++ phalcon/app/tasks/test/RedisTask.php
@@ -8,6 +8,7 @@
 // +----------------------------------------------------------------------
 namespace MyApp\Tasks\Test;
 
+use limx\phalcon\Redis;
 use limx\tools\LRedis;
 use Phalcon\Cli\Task;
 use limx\phalcon\DB;
@@ -27,17 +28,42 @@ class RedisTask extends Task
 
         echo Color::head('Actions:') . PHP_EOL;
         echo Color::colorize('  sadd            sadd测试', Color::FG_GREEN) . PHP_EOL;
+        echo Color::colorize('  hmget           hmget测试', Color::FG_GREEN) . PHP_EOL;
+        echo Color::colorize('  hget            hget测试', Color::FG_GREEN) . PHP_EOL;
     }
 
-    private function redisClient()
+    public function hgetAction()
     {
-        $config = di('config');
-        $redis = LRedis::getInstance([
-            'host' => $config->redis->host,
-            'auth' => $config->redis->auth,
-            'port' => $config->redis->port,
-        ]);
-        return $redis;
+        $redis = $this->redisClient();
+        $keys = ['key1' => uniqid(), 'key2' => uniqid(), 'key3' => uniqid(), 'key4' => uniqid()];
+        echo Color::head('KEY值为'), PHP_EOL;
+        echo Color::colorize('  ' . self::TEST_KEY, Color::FG_LIGHT_GREEN), PHP_EOL;
+        echo Color::head('设置测试数据：');
+        $redis->hmset(self::TEST_KEY, $keys);
+        echo Color::colorize('  设置成功', Color::FG_LIGHT_GREEN), PHP_EOL;
+
+        $res = $redis->hget(self::TEST_KEY, 'key1');
+        echo Color::head('存在时结果：');
+        echo Color::colorize(sprintf("  %s", json_encode($res)), Color::FG_LIGHT_GREEN), PHP_EOL;
+
+        $res = $redis->hget(self::TEST_KEY, 'key11');
+        echo Color::head('不存在时结果：');
+        echo Color::colorize(sprintf("  %s", json_encode($res)), Color::FG_LIGHT_GREEN), PHP_EOL;
+    }
+
+    public function hmgetAction()
+    {
+        $redis = $this->redisClient();
+        $keys = ['key1' => uniqid(), 'key2' => uniqid(), 'key3' => uniqid(), 'key4' => uniqid()];
+        echo Color::head('KEY值为') . PHP_EOL;
+        echo Color::colorize('  ' . self::TEST_KEY, Color::FG_LIGHT_GREEN), PHP_EOL;
+        echo Color::head('设置测试数据：');
+        $redis->hmset(self::TEST_KEY, $keys);
+        echo Color::colorize('  设置成功', Color::FG_LIGHT_GREEN), PHP_EOL;
+
+        $res = $redis->hmget(self::TEST_KEY, ['key1', 'key2', 'key3']);
+        echo Color::head('结果：');
+        echo Color::colorize(sprintf("  %s", json_encode($res)), Color::FG_LIGHT_GREEN), PHP_EOL;
     }
 
     public function saddAction()
@@ -52,5 +78,25 @@ class RedisTask extends Task
         $res = $redis->sadd(self::TEST_KEY, $val);
         echo Color::head('第二次结果：');
         echo Color::colorize('  ' . $res, Color::FG_LIGHT_GREEN) . PHP_EOL;
+    }
+
+    private function redisClient()
+    {
+        $config = di('config');
+        $redis = Redis::getInstance(
+            $config->redis->host,
+            $config->redis->auth,
+            $config->redis->index,
+            $config->redis->port
+        );
+        $redis->del(self::TEST_KEY);
+        return $redis;
+
+        $redis = LRedis::getInstance([
+            'host' => $config->redis->host,
+            'auth' => $config->redis->auth,
+            'port' => $config->redis->port,
+        ]);
+        return $redis;
     }
 }
