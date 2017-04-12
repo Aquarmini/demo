42cc01280cafb91f0889ea39be66020b9a1ba533
李铭昕 <715557344@qq.com>
2017-04-12 19:45:08 +0800

    * phalcon

--- phalcon/app/tasks/test/ArrayTask.php
+++ phalcon/app/tasks/test/ArrayTask.php
@@ -9,38 +9,58 @@ class ArrayTask extends Task
 {
     public function mainAction()
     {
-        echo Color::head('Help:') . PHP_EOL;
-        echo Color::colorize('  Array函数测试') . PHP_EOL . PHP_EOL;
+        echo Color::head('Help:'), PHP_EOL;
+        echo Color::colorize('  Array函数测试'), PHP_EOL, PHP_EOL;
 
-        echo Color::head('Usage:') . PHP_EOL;
-        echo Color::colorize('  php run Test\\\\Array [action]', Color::FG_GREEN) . PHP_EOL . PHP_EOL;
+        echo Color::head('Usage:'), PHP_EOL;
+        echo Color::colorize('  php run Test\\\\Array [action]', Color::FG_GREEN), PHP_EOL, PHP_EOL;
 
-        echo Color::head('Actions:') . PHP_EOL;
-        echo Color::colorize('  chunk                   数组重新分组', Color::FG_GREEN) . PHP_EOL;
-        echo Color::colorize('  case    [upper|lower]   修改键名为全大写或小写', Color::FG_GREEN) . PHP_EOL;
-        echo Color::colorize('  combine                 拼接key数组和val数组', Color::FG_GREEN) . PHP_EOL;
-        echo Color::colorize('  count                   统计数组中所有的值出现的次数', Color::FG_GREEN) . PHP_EOL;
-        echo Color::colorize('  diff                    计算数组差集', Color::FG_GREEN) . PHP_EOL;
-        echo Color::colorize('  fill                    用给定的值填充数组', Color::FG_GREEN) . PHP_EOL;
-        echo Color::colorize('  filter                  用回调函数过滤数组中的单元', Color::FG_GREEN) . PHP_EOL;
-        echo Color::colorize('  intersect               返回arr1中包含arr2中值的数组', Color::FG_GREEN) . PHP_EOL;
-        echo Color::colorize('  keys                    返回数组的key值', Color::FG_GREEN) . PHP_EOL;
-        echo Color::colorize('  map                     返回callback处理之后的数组', Color::FG_GREEN) . PHP_EOL;
-        echo Color::colorize('  unique                  数组去重', Color::FG_GREEN) . PHP_EOL;
-        echo Color::colorize('  mergeRecursive          合并多个数组，键值相同则合并成一个数组', Color::FG_GREEN) . PHP_EOL;
-        echo Color::colorize('  pad     [:len] [:val]   根据指定长度填充数组', Color::FG_GREEN) . PHP_EOL;
-        echo Color::colorize('  splice  [:off] [:len]   根据指定长度切割', Color::FG_GREEN) . PHP_EOL;
+        echo Color::head('Actions:'), PHP_EOL;
+        echo Color::colorize('  chunk                   数组重新分组', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  case    [upper|lower]   修改键名为全大写或小写', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  combine                 拼接key数组和val数组', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  count                   统计数组中所有的值出现的次数', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  diff                    计算数组差集', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  fill                    用给定的值填充数组', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  filter                  用回调函数过滤数组中的单元', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  intersect               返回arr1中包含arr2中值的数组', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  keys                    返回数组的key值', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  map                     返回callback处理之后的数组', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  unique                  数组去重', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  mergeRecursive          合并多个数组，键值相同则合并成一个数组', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  pad     [:len] [:val]   根据指定长度填充数组', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  splice  [:off] [:len]   根据指定长度切割', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  multisort               数组排序', Color::FG_GREEN), PHP_EOL;
 
+    }
+
+    public function multisortAction()
+    {
+        $arr = [];
+        $sort1 = [];
+        $sort2 = [];
+        for ($i = 0; $i < 100; $i++) {
+            $s1 = rand(1, 10);
+            $s2 = rand(1, 100);
+            $arr[] = ['sort1' => $s1, 'sort2' => $s2, 'val' => uniqid()];
+            $sort1[] = $s1;
+            $sort2[] = $s2;
+        }
+        echo Color::head("原数组arr："), PHP_EOL;
+        echo Color::colorize(json_encode($arr), Color::FG_LIGHT_GREEN), PHP_EOL;
+        echo Color::head("结果："), PHP_EOL;
+        array_multisort($sort1, SORT_DESC, $sort2, SORT_ASC, $arr);
+        echo Color::colorize(json_encode($arr), Color::FG_LIGHT_GREEN), PHP_EOL;
     }
 
     public function uniqueAction()
     {
         $arr = [1, 1, 222, 222, 3, 4, 5];
-        echo Color::head("原数组arr：") . PHP_EOL;
-        echo Color::colorize(json_encode($arr), Color::FG_LIGHT_GREEN) . PHP_EOL;
-        echo Color::head("结果：") . PHP_EOL;
+        echo Color::head("原数组arr："), PHP_EOL;
+        echo Color::colorize(json_encode($arr), Color::FG_LIGHT_GREEN), PHP_EOL;
+        echo Color::head("结果："), PHP_EOL;
         $res = array_unique($arr);
-        echo Color::colorize(json_encode($res), Color::FG_LIGHT_GREEN) . PHP_EOL;
+        echo Color::colorize(json_encode($res), Color::FG_LIGHT_GREEN), PHP_EOL;
     }
 
     public function spliceAction($params)
@@ -51,14 +71,14 @@ class ArrayTask extends Task
         }
 
         $arr = [1, 2, 3, 4, 5];
-        echo Color::head("原数组arr：") . PHP_EOL;
-        echo Color::colorize(json_encode($arr), Color::FG_LIGHT_GREEN) . PHP_EOL;
-        echo Color::head("结果：") . PHP_EOL;
+        echo Color::head("原数组arr："), PHP_EOL;
+        echo Color::colorize(json_encode($arr), Color::FG_LIGHT_GREEN), PHP_EOL;
+        echo Color::head("结果："), PHP_EOL;
         $res = array_splice($arr, $params[0], $params[1]);
-        echo Color::head("新数组res：") . PHP_EOL;
-        echo Color::colorize(json_encode($res), Color::FG_LIGHT_GREEN) . PHP_EOL;
-        echo Color::head("原数组arr：") . PHP_EOL;
-        echo Color::colorize(json_encode($arr), Color::FG_LIGHT_GREEN) . PHP_EOL;
+        echo Color::head("新数组res："), PHP_EOL;
+        echo Color::colorize(json_encode($res), Color::FG_LIGHT_GREEN), PHP_EOL;
+        echo Color::head("原数组arr："), PHP_EOL;
+        echo Color::colorize(json_encode($arr), Color::FG_LIGHT_GREEN), PHP_EOL;
     }
 
     public function padAction($params)
@@ -69,68 +89,68 @@ class ArrayTask extends Task
         }
 
         $arr = [1, 2, 3, 4, 5];
-        echo Color::head("原数组：") . PHP_EOL;
-        echo Color::colorize(json_encode($arr), Color::FG_LIGHT_GREEN) . PHP_EOL;
-        echo Color::head("结果：") . PHP_EOL;
+        echo Color::head("原数组："), PHP_EOL;
+        echo Color::colorize(json_encode($arr), Color::FG_LIGHT_GREEN), PHP_EOL;
+        echo Color::head("结果："), PHP_EOL;
         $res = array_pad($arr, $params[0], $params[1]);
-        echo Color::colorize(json_encode($res), Color::FG_LIGHT_GREEN) . PHP_EOL;
+        echo Color::colorize(json_encode($res), Color::FG_LIGHT_GREEN), PHP_EOL;
     }
 
     public function mergeRecursiveAction()
     {
         $arr1 = ["color" => ["favorite" => "red"], 5];
         $arr2 = [10, "color" => ["favorite" => "green", "blue"]];
-        echo Color::head("原数组1：") . PHP_EOL;
-        echo Color::colorize(json_encode($arr1), Color::FG_LIGHT_GREEN) . PHP_EOL;
-        echo Color::head("原数组2：") . PHP_EOL;
-        echo Color::colorize(json_encode($arr2), Color::FG_LIGHT_GREEN) . PHP_EOL;
-        echo Color::head("结果：") . PHP_EOL;
+        echo Color::head("原数组1："), PHP_EOL;
+        echo Color::colorize(json_encode($arr1), Color::FG_LIGHT_GREEN), PHP_EOL;
+        echo Color::head("原数组2："), PHP_EOL;
+        echo Color::colorize(json_encode($arr2), Color::FG_LIGHT_GREEN), PHP_EOL;
+        echo Color::head("结果："), PHP_EOL;
         $res = array_merge_recursive($arr1, $arr2);
-        echo Color::colorize(json_encode($res), Color::FG_LIGHT_GREEN) . PHP_EOL;
+        echo Color::colorize(json_encode($res), Color::FG_LIGHT_GREEN), PHP_EOL;
     }
 
     public function mapAction()
     {
         $arr = [1, 2, 3, 4, 5];
-        echo Color::head("原数组：") . PHP_EOL;
-        echo Color::colorize(json_encode($arr), Color::FG_LIGHT_GREEN) . PHP_EOL;
-        echo Color::head("结果：") . PHP_EOL;
+        echo Color::head("原数组："), PHP_EOL;
+        echo Color::colorize(json_encode($arr), Color::FG_LIGHT_GREEN), PHP_EOL;
+        echo Color::head("结果："), PHP_EOL;
         $res = array_map(function ($n) {
             return $n * $n;
         }, $arr);
-        echo Color::colorize(json_encode($res), Color::FG_LIGHT_GREEN) . PHP_EOL;
+        echo Color::colorize(json_encode($res), Color::FG_LIGHT_GREEN), PHP_EOL;
     }
 
     public function keysAction()
     {
         $arr = array("a" => "green", "red", "blue", "b" => "green", 22 => "yellow", "red");
-        echo Color::head("原数组1：") . PHP_EOL;
-        echo Color::colorize(json_encode($arr), Color::FG_LIGHT_GREEN) . PHP_EOL;
-        echo Color::head("结果：") . PHP_EOL;
-        echo Color::colorize(json_encode(array_keys($arr)), Color::FG_LIGHT_GREEN) . PHP_EOL;
+        echo Color::head("原数组1："), PHP_EOL;
+        echo Color::colorize(json_encode($arr), Color::FG_LIGHT_GREEN), PHP_EOL;
+        echo Color::head("结果："), PHP_EOL;
+        echo Color::colorize(json_encode(array_keys($arr)), Color::FG_LIGHT_GREEN), PHP_EOL;
     }
 
     public function intersectAction()
     {
         $arr1 = array("a" => "green", "red", "blue");
         $arr2 = array("b" => "green", "yellow", "red");
-        echo Color::head("原数组1：") . PHP_EOL;
-        echo Color::colorize(json_encode($arr1), Color::FG_LIGHT_GREEN) . PHP_EOL;
-        echo Color::head("原数组2：") . PHP_EOL;
-        echo Color::colorize(json_encode($arr2), Color::FG_LIGHT_GREEN) . PHP_EOL;
-        echo Color::head("结果：") . PHP_EOL;
-        echo Color::colorize(json_encode(array_intersect($arr1, $arr2)), Color::FG_LIGHT_GREEN) . PHP_EOL;
+        echo Color::head("原数组1："), PHP_EOL;
+        echo Color::colorize(json_encode($arr1), Color::FG_LIGHT_GREEN), PHP_EOL;
+        echo Color::head("原数组2："), PHP_EOL;
+        echo Color::colorize(json_encode($arr2), Color::FG_LIGHT_GREEN), PHP_EOL;
+        echo Color::head("结果："), PHP_EOL;
+        echo Color::colorize(json_encode(array_intersect($arr1, $arr2)), Color::FG_LIGHT_GREEN), PHP_EOL;
     }
 
     public function filterAction()
     {
         $arr = [1, 2, 3, 'a' => 4, 5, 6, 7, 'b' => 9, 8, 10];
-        echo Color::head("原数组：") . PHP_EOL;
-        echo Color::colorize(json_encode($arr), Color::FG_LIGHT_GREEN) . PHP_EOL;
-        echo Color::head("结果：") . PHP_EOL;
+        echo Color::head("原数组："), PHP_EOL;
+        echo Color::colorize(json_encode($arr), Color::FG_LIGHT_GREEN), PHP_EOL;
+        echo Color::head("结果："), PHP_EOL;
         echo Color::colorize(json_encode(array_filter($arr, function ($var) {
-                return ($var & 1);
-            })), Color::FG_LIGHT_GREEN) . PHP_EOL;
+            return ($var & 1);
+        })), Color::FG_LIGHT_GREEN), PHP_EOL;
 
     }
 
@@ -148,37 +168,37 @@ class ArrayTask extends Task
             return false;
         }
         $res = array_fill($begin, $len, $val);
-        echo Color::colorize(json_encode($res), Color::FG_LIGHT_GREEN) . PHP_EOL;
+        echo Color::colorize(json_encode($res), Color::FG_LIGHT_GREEN), PHP_EOL;
     }
 
     public function diffAction()
     {
         $arr1 = [1, 2, 3, '4' => 4, '5' => 5];
         $arr2 = [2, 3, '5' => 5, '6' => 5];
-        echo Color::head("原数组：") . PHP_EOL;
-        echo Color::colorize(json_encode($arr1), Color::FG_LIGHT_GREEN) . PHP_EOL;
-        echo Color::colorize(json_encode($arr2), Color::FG_LIGHT_GREEN) . PHP_EOL;
-        echo Color::head("结果：") . PHP_EOL;
-        echo Color::colorize(json_encode(array_diff($arr1, $arr2)), Color::FG_LIGHT_GREEN) . PHP_EOL;
+        echo Color::head("原数组："), PHP_EOL;
+        echo Color::colorize(json_encode($arr1), Color::FG_LIGHT_GREEN), PHP_EOL;
+        echo Color::colorize(json_encode($arr2), Color::FG_LIGHT_GREEN), PHP_EOL;
+        echo Color::head("结果："), PHP_EOL;
+        echo Color::colorize(json_encode(array_diff($arr1, $arr2)), Color::FG_LIGHT_GREEN), PHP_EOL;
     }
 
     public function countAction()
     {
         $data = [1, 2, 'hello', 1, 'hello', 'hello', '1'];
-        echo Color::head("原数组：") . PHP_EOL;
-        echo Color::colorize(json_encode($data), Color::FG_LIGHT_GREEN) . PHP_EOL;
-        echo Color::colorize(json_encode(array_count_values($data)), Color::FG_LIGHT_GREEN) . PHP_EOL;
+        echo Color::head("原数组："), PHP_EOL;
+        echo Color::colorize(json_encode($data), Color::FG_LIGHT_GREEN), PHP_EOL;
+        echo Color::colorize(json_encode(array_count_values($data)), Color::FG_LIGHT_GREEN), PHP_EOL;
     }
 
     public function combineAction()
     {
         $key = ['key1', 'key2', 'key3'];
         $val = ['val1', 'val2', 'val3'];
-        echo Color::head("原数组：") . PHP_EOL;
-        echo Color::colorize(json_encode($key), Color::FG_LIGHT_GREEN) . PHP_EOL;
-        echo Color::colorize(json_encode($val), Color::FG_LIGHT_GREEN) . PHP_EOL;
-        echo Color::head("结果：") . PHP_EOL;
-        echo Color::colorize(json_encode(array_combine($key, $val)), Color::FG_LIGHT_GREEN) . PHP_EOL;
+        echo Color::head("原数组："), PHP_EOL;
+        echo Color::colorize(json_encode($key), Color::FG_LIGHT_GREEN), PHP_EOL;
+        echo Color::colorize(json_encode($val), Color::FG_LIGHT_GREEN), PHP_EOL;
+        echo Color::head("结果："), PHP_EOL;
+        echo Color::colorize(json_encode(array_combine($key, $val)), Color::FG_LIGHT_GREEN), PHP_EOL;
     }
 
     public function caseAction($params)
@@ -189,23 +209,23 @@ class ArrayTask extends Task
         }
         $type = $params[0] == 'upper' ? CASE_UPPER : CASE_LOWER;
         $data = ['test' => 'tt', 'Tes' => 123, 'AA' => 'sdf'];
-        echo Color::head("原数组：") . PHP_EOL;
-        echo Color::colorize("  " . json_encode($data), Color::FG_LIGHT_GREEN) . PHP_EOL;
-        echo Color::head("array_change_key_case(data,type)") . PHP_EOL;
-        echo Color::colorize("  " . json_encode(array_change_key_case($data, $type)), Color::FG_LIGHT_GREEN) . PHP_EOL;
+        echo Color::head("原数组："), PHP_EOL;
+        echo Color::colorize("  " . json_encode($data), Color::FG_LIGHT_GREEN), PHP_EOL;
+        echo Color::head("array_change_key_case(data,type)"), PHP_EOL;
+        echo Color::colorize("  " . json_encode(array_change_key_case($data, $type)), Color::FG_LIGHT_GREEN), PHP_EOL;
     }
 
     public function chunkAction()
     {
         $arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-        echo Color::head("原数组：") . PHP_EOL;
-        echo Color::colorize("  " . json_encode($arr), Color::FG_LIGHT_GREEN) . PHP_EOL;
+        echo Color::head("原数组："), PHP_EOL;
+        echo Color::colorize("  " . json_encode($arr), Color::FG_LIGHT_GREEN), PHP_EOL;
         $res = array_chunk($arr, 3);
-        echo Color::head("3个一组分组：") . PHP_EOL;
-        echo Color::colorize("  " . json_encode($res), Color::FG_LIGHT_GREEN) . PHP_EOL;
+        echo Color::head("3个一组分组："), PHP_EOL;
+        echo Color::colorize("  " . json_encode($res), Color::FG_LIGHT_GREEN), PHP_EOL;
         $res = array_chunk($arr, 20);
-        echo Color::head("20个一组分组：") . PHP_EOL;
-        echo Color::colorize("  " . json_encode($res), Color::FG_LIGHT_GREEN) . PHP_EOL;
+        echo Color::head("20个一组分组："), PHP_EOL;
+        echo Color::colorize("  " . json_encode($res), Color::FG_LIGHT_GREEN), PHP_EOL;
     }
 
 
--- phalcon/app/tasks/test/MainTask.php
+++ phalcon/app/tasks/test/MainTask.php
@@ -17,24 +17,25 @@ class MainTask extends Task
 {
     public function mainAction()
     {
-        echo Color::head('Help:') . PHP_EOL;
-        echo Color::colorize('  测试脚本列表') . PHP_EOL . PHP_EOL;
+        echo Color::head('Help:'), PHP_EOL;
+        echo Color::colorize('  测试脚本列表'), PHP_EOL, PHP_EOL;
 
-        echo Color::head('Usage:') . PHP_EOL;
-        echo Color::colorize('  php run Test\\\\[task]', Color::FG_GREEN) . PHP_EOL . PHP_EOL;
-        echo Color::head('Tasks:') . PHP_EOL;
-        echo Color::colorize('  Arg                     php函数参数相关测试', Color::FG_GREEN) . PHP_EOL;
-        echo Color::colorize('  Array                   array函数相关测试', Color::FG_GREEN) . PHP_EOL;
-        echo Color::colorize('  Bitwise                 位运算相关测试', Color::FG_GREEN) . PHP_EOL;
-        echo Color::colorize('  Curl                    Curl函数相关测试', Color::FG_GREEN) . PHP_EOL;
-        echo Color::colorize('  Compare                 比较测试', Color::FG_GREEN) . PHP_EOL;
-        echo Color::colorize('  OAuth                   OAuth函数相关测试', Color::FG_GREEN) . PHP_EOL;
-        echo Color::colorize('  Ymal                    Ymal函数相关测试', Color::FG_GREEN) . PHP_EOL;
-        echo Color::colorize('  Sort                    排序算法测试', Color::FG_GREEN) . PHP_EOL;
-        echo Color::colorize('  Sphinx                  Sphinx全文检索引擎', Color::FG_GREEN) . PHP_EOL;
-        echo Color::colorize('  Match                   正则表达式测试', Color::FG_GREEN) . PHP_EOL;
-        echo Color::colorize('  Mysql                   Mysql测试', Color::FG_GREEN) . PHP_EOL;
-        echo Color::colorize('  Redis                   Redis测试', Color::FG_GREEN) . PHP_EOL;
+        echo Color::head('Usage:'), PHP_EOL;
+        echo Color::colorize('  php run Test\\\\[task]', Color::FG_GREEN), PHP_EOL, PHP_EOL;
+        echo Color::head('Tasks:'), PHP_EOL;
+        echo Color::colorize('  Arg                     php函数参数相关测试', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  Array                   array函数相关测试', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  Bitwise                 位运算相关测试', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  Compare                 比较测试', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  Curl                    Curl函数相关测试', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  DB                      DB相关测试', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  OAuth                   OAuth函数相关测试', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  Ymal                    Ymal函数相关测试', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  Sort                    排序算法测试', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  Sphinx                  Sphinx全文检索引擎', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  Match                   正则表达式测试', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  Mysql                   Mysql测试', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  Redis                   Redis测试', Color::FG_GREEN), PHP_EOL;
     }
 
 }
