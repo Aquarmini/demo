23499575a561810dc6d3bbdf4a1d900b9befd627
李铭昕 <715557344@qq.com>
2017-05-21 01:27:20 +0800

    * laravel

--- laravel/5.4/app/Http/Controllers/IndexController.php
+++ laravel/5.4/app/Http/Controllers/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -14,7 +15,13 @@ class IndexController extends Controller
 
     public function sql()
     {
-        $res = DB::select("SELECT * FROM user WHERE id in (?,?);", [1, 2]);
+        // $res = DB::select("SELECT * FROM user WHERE id in (?,?);", [1, 2]);
+        // foreach ($res as $user) {
+        //     dump($user->name);
+        // }
+
+        // $res = DB::table('user')->whereIn('id', [1, 2, 3])->get();
+        $res = User::whereIn('id', [1, 2, 3])->get();
         foreach ($res as $user) {
             dump($user->name);
         }
