cea85869a568e3c29b9872daf448389efa96c0b2
李铭昕 <715557344@qq.com>
2017-06-01 16:30:46 +0800

    * phalcon

--- phalcon/app/tasks/test/TestTask.php
+++ phalcon/app/tasks/test/TestTask.php
@@ -24,7 +24,37 @@ class TestTask extends Task
 
         echo Color::head('Actions:'), PHP_EOL;
         echo Color::colorize('  sleep       延时脚本', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  switch      switch测试', Color::FG_GREEN), PHP_EOL;
     }
+
+    public function switchAction($params = [])
+    {
+        $case = 0;
+        if (count($params) > 0) {
+            $case = $params[0];
+        }
+
+        switch (true) {
+            case $case == 0:
+                $msg = "case=0";
+                break;
+            case $case == 1:
+                $msg = "case=1";
+                break;
+            case $case == 2:
+                $msg = "case=2";
+                break;
+            case $case == 3:
+                $msg = "case=3";
+                break;
+            default:
+                $msg = "case not in (0,1,2,3)";
+                break;
+        }
+
+        echo Color::colorize($msg, Color::FG_GREEN);
+    }
+
 
     public function sleepAction($params)
     {
