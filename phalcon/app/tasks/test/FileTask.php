192d35c3bce1ff5d765a71f6464f77002dbf976c
李铭昕 <715557344@qq.com>
2017-06-28 23:40:23 +0800

    * phalcon

--- phalcon/app/tasks/test/FileTask.php
+++ phalcon/app/tasks/test/FileTask.php
@@ -28,6 +28,49 @@ class FileTask extends Task
         echo Color::colorize('  csv         CSV文件解析', Color::FG_GREEN), PHP_EOL;
         echo Color::colorize('  csvVol      CSV文件电压解析', Color::FG_GREEN), PHP_EOL;
         echo Color::colorize('  log         日志存储', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  image       图片裁剪', Color::FG_GREEN), PHP_EOL;
+    }
+
+    public function imageAction()
+    {
+        $file = ROOT_PATH . '/data/file/logo.png';
+        $output = ROOT_PATH . '/data/file/logo1.png';
+        $res = $this->imageResize($file, 500, 800, $output);
+        print_r($res);
+
+        $output = ROOT_PATH . '/data/file/logo2.png';
+        $res = $this->imageResize($file, 500, 200, $output);
+        print_r($res);
+    }
+
+    protected function imageResize($file, $width = 500, $height = 500, $output = null)
+    {
+        $image = new \Phalcon\Image\Adapter\Gd($file);
+        $r_width = null;
+        $r_height = null;
+
+        $r1 = floatval($image->getWidth() / $image->getHeight());
+        $r2 = floatval($width / $height);
+        Log::info(json_encode([$r1, $r2]));
+        if ($r1 > $r2) {
+
+            $offsetX = intval((floatval($image->getWidth() / $image->getHeight()) * $height - $width) / 2);
+            $offsetY = 0;
+            $r_height = $height;  //
+            $master = \Phalcon\Image::HEIGHT;  //
+        } else {
+            $offsetX = 0;
+            $offsetY = intval(floatval(($image->getHeight() / $image->getWidth()) * $width - $height) / 2);
+            $r_width = $width;
+            $master = \Phalcon\Image::WIDTH;
+        }
+        Log::info(json_encode([$r_width, $r_height, $master, $width, $height, $offsetX, $offsetY]));
+        if (!isset($output)) {
+            $output = $file;
+        }
+        return $image->resize($r_width, $r_height, $master)
+            ->crop($width, $height, $offsetX, $offsetY)
+            ->save($output);
     }
 
     public function csvVolAction()
